e6b808ccf5e601b3d0c55c458f9718a5bafd8f71
Jamie Guo <jamie.guo@okta.com>
2022-05-26 17:50:24 -0400

    Add granular consent view on OIE
    Resolves: OKTA-487625

--- playground/mocks/config/responseConfig.js
+++ playground/mocks/config/responseConfig.js
@@ -220,6 +220,7 @@ const consent = {
   '/idp/idx/introspect': [
     // 'consent-admin',
     'consent-enduser',
+    'consent-granular',
   ],
   '/idp/idx/consent': [
     // note that the success 'href' is in reality a redirect (i.e. /login/token/redirect?stateToken={{stateToken}})
