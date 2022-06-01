4eab0b50e211992c0c82573cbfadafa815354c7e
lesterchoi <lester.choi@okta.com>
2022-06-01 13:59:56 -0400

    consolidate auth-js dependency, fix build error in v3 script

--- src/v3/preact.config.js
+++ src/v3/preact.config.js
@@ -19,29 +19,30 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import envVars from 'preact-cli-plugin-env-vars';
 import { DefinePlugin } from 'webpack';
 
-const PROJECT_ROOT = resolve(__dirname, '../../');
-const TARGET = resolve(PROJECT_ROOT, 'target');
-const PLAYGROUND = resolve(PROJECT_ROOT, 'playground');
+// util: resolve paths relative to the project root
+const rootResolve = (...segments) => resolve(__dirname, '../../', ...segments);
+
+const TARGET = rootResolve('target');
+const PLAYGROUND = rootResolve('playground');
 const DEV_SERVER_PORT = 3000;
 const MOCK_SERVER_PORT = 3030;
-const WIDGET_RC_JS = resolve(PROJECT_ROOT, '.widgetrc.js');
-const WIDGET_RC = resolve(PROJECT_ROOT, '.widgetrc');
+const WIDGET_RC_JS = rootResolve('.widgetrc.js');
+const WIDGET_RC = rootResolve('.widgetrc');
 
 // run `OKTA_SIW_HOST=0.0.0.0 yarn start --watch` to override the host
 const HOST = process.env.OKTA_SIW_HOST || 'localhost';
 
 if (!existsSync(WIDGET_RC_JS) && existsSync(WIDGET_RC)) {
-  console.error('============================================');
-  console.error(`Please migrate the ${WIDGET_RC} to ${WIDGET_RC_JS}.`);
-  /* eslint-disable-next-line @okta/okta/no-exclusive-language */
-  console.error('For more information, please see https://github.com/okta/okta-signin-widget/blob/master/MIGRATING.md');
-  console.error('============================================');
+  // eslint-disable-next-line @okta/okta/no-exclusive-language
+  console.error(`============================================
+Please migrate the ${WIDGET_RC} to ${WIDGET_RC_JS}.
+For more information, please see
+https://github.com/okta/okta-signin-widget/blob/master/MIGRATING.md
+============================================`);
   process.exit(1);
-}
-
-if (!existsSync(WIDGET_RC_JS)) {
+} else if (!existsSync(WIDGET_RC_JS)) {
   // create default WIDGET_RC if it doesn't exist to simplifed the build process
-  copyFileSync('.widgetrc.sample.js', WIDGET_RC_JS);
+  copyFileSync(rootResolve('.widgetrc.sample.js'), WIDGET_RC_JS);
 }
 
 export default {
